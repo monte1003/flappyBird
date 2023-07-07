89cfd28e02559514bf65c29bd81a9aa23f91854b
unknown <isaacmonterrosaguzman@gmail.com>
2023-07-07 17:54:01 -0500

    probando juego

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Juego Flappy Birld
 ## Canvas
 - La etiqueta canvas sirve para graficos, y a su vez animarlos
+
+## COntext
+- Cambia dentro del canvas
+
+## Assets
+
+- Componentes de un juego, programa, etc.
--- js/game.js
+++ js/game.js
@@ -1,30 +1,121 @@
-let contex=document.getElementById('gameCanvas').getContext('2d')
-contex.canvas.width = 300
-contex.canvas.height = 700
+let canvas = document.getElementById("gameCanvas")
+let context = canvas.getContext("2d")
 
-let gravedad=1.5}
+let width = 300
+let height = 530
+let canvasWidth = 300
+let canvasHeight = 530
+
+let gameOver=false;
+
+canvas.width = width
+canvas.height = height
+
+let gravity=1.5
 let fps=60
 
-let bird={
-    x:150,
-    y:100,
-    w:50,
-    h:50
-}
-setInterval(loop, 1000/fps)/*Sirve para ejecutrar la función - 60 CUADRos POR SEGUNDO */
-function loop(){
-    contex.clearRect(0,0,300,700)/*Posición del objeto con respecto a las medidas del canvas*/ /* Crea un nuevo cuadro y limpia el antterior */
-    contex.fillStyle = 'rgba(100,45,300,1)'
-    contex.fillRect(bird.x,bird.y,bird.w,bird.h)
+// Assets
+let birdImage= new Image()
+birdImage.src="img/bird.png"
 
-    bird.y+=gravedad
+let background= new Image()
+background.src="img/background.png"
+
+let floor= new Image()
+floor.src="img/descarga.png"
+
+let northPipe= new Image()
+northPipe.src="img/tuberiaNorte.png"
+
+let southPipe= new Image()
+southPipe.src="img/tuberiaSur.png"
+
+let spot = new Audio()
+spot.src = "audios/punto.mp3"
+
+
+let bird={
+  x: 50,
+  y: 150,
+  w: 50,
+  h: 50,
 }
+
+let pipes = new Array()
+pipes[0] = {
+  x: canvas.width,
+  y: 0,
+}
+
+setInterval(loop, 1000 / fps)
+function loop() {
+  // Clear
+    context.clearRect(0, 0, 300, 700)
+  // Background
+    context.drawImage(background, 0, 0)
+    context.drawImage(floor, 0, canvas.height - floor.height)
+  // Bird
+    context.drawImage(birdImage, bird.x, bird.y)
+  // Pipes
+    for (var i = 0; i < pipes.length; i++) {
+    let constant = northPipe.height + 90
+    context.drawImage(northPipe, pipes[i].x, pipes[i].y)
+    context.drawImage(southPipe, pipes[i].x, pipes[i].y + constant)
+
+    // Pipe behavior
+    pipes[i].x--
+    if (pipes[i].y + northPipe.height < 80) {
+      pipes[i].y = 0
+    }
+    if (pipes[i].x == 150) {
+      pipes.push({
+        x: canvas.width,
+        y: Math.floor(Math.random() * northPipe.height) - northPipe.height,
+      })
+    }
+
+    if (
+      (bird.x + birdImage.width >= pipes[i].x &&
+        bird.x <= pipes[i].x + northPipe.width &&
+        (bird.y <= pipes[i].y + northPipe.height ||
+          bird.y + birdImage.height >= pipes[i].y + constant)) ||
+      bird.y + birdImage.height >= canvas.height - floor.height
+    ) {
+      window.location.reload()
+    }
+
+    if (pipes[i].x == bird.x) {
+      score++
+      spot.play()
+    }
+  }
+
+  bird.y += gravity
+  context.fillstyle = "rgba(0,0,0,1)"
+  context.font = "25px Arial"
+  context.fillText("Score: " + score, 10, canvas.height - 30)
+}
+
 
 //CONTROLES
 
-function press(){
-    bird.y-=10
-
+function press() {
+  bird.y -= 25
 }
 
-window.addEventListener('keydown',press)
+//resize()
+function resize() {
+  canvasHeight = window.innerHeight
+  canvasWidth = window.innerWidth
+
+  canvas.width = width
+  canvas.height = height
+
+  canvas.style.height = `${canvasHeight}px`
+}
+function pause(){
+  location.pause()
+}
+
+window.addEventListener("keydown", press)
+window.addEventListener("resize", resize)
